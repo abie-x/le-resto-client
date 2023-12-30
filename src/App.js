ba7ab1112121057f66375d9fccb84d329c6e854f
abie_x <abhiramzmenon@gmail.com>
2023-12-30 20:40:37 +0530

    finished desktop version of home, signup and login screens

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import LoginScreen from './screens/LoginScreen';
+import SplashScreen from './screens/SplashScreen';
+import SignupScreen from './screens/SignupScreen';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+          <Route path='/' Component={SplashScreen} />
+          <Route path='/login' Component={LoginScreen} />
+          <Route path='/signup' Component={SignupScreen} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+const Navbar = () => {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
+
+    return (
+        <nav className="">
+            <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                <div className="flex items-center justify-between h-16">
+                    {/* Logo */}
+                    <div className="flex-shrink-0">
+                        <img className="h-5 w-auto" src="logo.png" alt="Your Company" />
+                    </div>
+                    {/* Nav Links */}
+                    <div className="hidden sm:block">
+                        <div className="flex space-x-4">
+                            <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Home</a>
+                            <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contact</a>
+                        </div>
+                    </div>
+                    {/* Mobile Hamburger Menu */}
+                    <div className="sm:hidden">
+                        <button
+                            type="button"
+                            className="text-gray-400 hover:text-white focus:outline-none"
+                            aria-controls="mobile-menu"
+                            aria-expanded={isMobileMenuOpen}
+                            onClick={toggleMobileMenu}
+                        >
+                            <span className="sr-only">Open main menu</span>
+                            <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                {isMobileMenuOpen ? (
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                ) : (
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16m-7 6h7" />
+                                )}
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            {/* Mobile Menu */}
+            <div className={`sm:hidden ${isMobileMenuOpen ? 'block' : 'hidden'}`} id="mobile-menu">
+                <div className="px-2 pt-2 pb-3 space-y-1">
+                    <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Home</a>
+                    <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Contact</a>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
